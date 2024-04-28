0aced2ad33ffa4c272f7e301905de47c75fed81e
nua <enuesaa@gmail.com>
2024-04-28 19:45:36 +0900

    feat(serve): find pkg with checking name suffix `.leadblend.zip`

--- pkg/usecase/pkgopen.go
+++ pkg/usecase/pkgopen.go
@@ -2,43 +2,56 @@ package usecase
 
 import (
 	"archive/zip"
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/enuesaa/leadblend/pkg/repository"
 )
 
 func OpenPkg(repos repository.Repos) error {
-	zr, err := zip.OpenReader("a.zip")
+	filenames, err := repos.Fs.ListFiles(".")
 	if err != nil {
 		return err
 	}
-	defer zr.Close()
-
-	if err := repos.Fs.CreateDir(".a"); err != nil {
-		return err
-	}
-
-	for _, f := range zr.File {
-		reader, err := f.Open()
-		if err != nil {
-			return err
-		}
-		defer reader.Close()
-
-		if f.FileInfo().IsDir() {
+	for _, filename := range filenames {
+		if !strings.HasSuffix(filename, ".leadblend.zip") {
 			continue
 		}
+		unarchivedir := fmt.Sprintf(".%s", strings.ReplaceAll(filename, ".zip", ""))
 
-		path := filepath.Join(".a", filepath.Base(f.Name)) // remove parent dir name
-		realwriter, err := os.Create(path)
+		zr, err := zip.OpenReader(filename)
 		if err != nil {
 			return err
 		}
-		defer realwriter.Close()
-		if _,  err := io.Copy(realwriter, reader); err != nil {
+		defer zr.Close()
+
+		if err := repos.Fs.CreateDir(unarchivedir); err != nil {
 			return err
+		}
+
+		for _, f := range zr.File {
+			reader, err := f.Open()
+			if err != nil {
+				return err
+			}
+			defer reader.Close()
+
+			if f.FileInfo().IsDir() {
+				continue
+			}
+
+			path := filepath.Join(unarchivedir, filepath.Base(f.Name)) // remove parent dir name
+			realwriter, err := os.Create(path)
+			if err != nil {
+				return err
+			}
+			defer realwriter.Close()
+			if _,  err := io.Copy(realwriter, reader); err != nil {
+				return err
+			}
 		}
 	}
 
