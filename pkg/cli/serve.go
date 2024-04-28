7b8a94c5f1c3401c95249bc18cd15c62dc4539ea
nua <enuesaa@gmail.com>
2024-04-28 16:22:32 +0900

    feat(serve): use echo

--- pkg/cli/serve.go
+++ pkg/cli/serve.go
@@ -2,6 +2,9 @@ package cli
 
 import (
 	"github.com/enuesaa/leadblend/pkg/repository"
+	"github.com/enuesaa/leadblend/ui"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/spf13/cobra"
 )
 
@@ -10,7 +13,18 @@ func CreateServeCmd(repos repository.Repos) *cobra.Command {
 		Use:   "serve",
 		Short: "serve",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return nil
+			app := echo.New()
+			app.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+				Format: "method=${method}, uri=${uri}, status=${status}\n",
+			}))
+			app.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+				AllowOrigins: []string{"http://localhost:3001"},
+			}))
+
+			// ui
+			app.Any("/*", ui.Serve)
+
+			return app.Start(":3000")
 		},
 	}
 
