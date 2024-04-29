f63a7f133c10325c60f035a5b22579a66ee161a9
nua <enuesaa@gmail.com>
2024-04-29 23:54:09 +0900

    feat(graphql): try implement graphql subscription handler

--- pkg/graph/serve.go
+++ pkg/graph/serve.go
@@ -2,6 +2,7 @@ package graph
 
 import (
 	_ "embed"
+	"net/http"
 
 	"github.com/enuesaa/leadblend/pkg/repository"
 	"github.com/labstack/echo/v4"
@@ -10,6 +11,10 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 	graphql "github.com/graph-gophers/graphql-go"
 	"github.com/graph-gophers/graphql-go/relay"
+
+	"fmt"
+
+	"github.com/gorilla/websocket"
 )
 
 //go:embed query.gql
@@ -29,8 +34,35 @@ func Serve(repos repository.Repos) error {
 	}))
 	// This is for the graphql subscription.
 	// however gophers-graphql does not supported enoughly, so currently not using.
-	// app.GET("/graphql")
+	app.GET("/graphql", wshandle)
 	app.GET("/graphql/playground", echo.WrapHandler(playground.Handler("graphql", "/graphql")))
 
-	return app.Start(":3000")
+	return app.Start(":3002")
+}
+
+var (
+	upgrader = websocket.Upgrader{}
+)
+
+func wshandle(c echo.Context) error {
+	header := http.Header{}
+	header.Add("Sec-Websocket-Protocol", "graphql-transport-ws")
+	ws, err := upgrader.Upgrade(c.Response(), c.Request(), header)
+	if err != nil {
+		return err
+	}
+	defer ws.Close()
+
+	for {
+		_, msg, err := ws.ReadMessage()
+		if err != nil {
+			c.Logger().Error(err)
+		}
+		fmt.Printf("%s\n", msg)
+		if string(msg) == `{"type":"connection_init","payload":{}}` {
+			if err := ws.WriteMessage(websocket.TextMessage, []byte(`{"type":"connection_ack"}`)); err != nil {
+				c.Logger().Error(err)
+			}
+		}
+	}
 }
