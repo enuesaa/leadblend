a409978ce757f75c686f0742d08e7f55bd2b7af8
nua <enuesaa@gmail.com>
2024-04-30 00:08:44 +0900

    feat(graohql): subscription でやり取りできるよう雑に実装した

--- pkg/graph/serve.go
+++ pkg/graph/serve.go
@@ -1,7 +1,9 @@
 package graph
 
 import (
+	"context"
 	_ "embed"
+	"encoding/json"
 	"net/http"
 
 	"github.com/enuesaa/leadblend/pkg/repository"
@@ -44,6 +46,8 @@ var (
 	upgrader = websocket.Upgrader{}
 )
 
+// see https://fits.hatenablog.com/entry/2021/01/12/210926
+// see https://docs.aws.amazon.com/ja_jp/appsync/latest/devguide/real-time-websocket-client.html
 func wshandle(c echo.Context) error {
 	header := http.Header{}
 	header.Add("Sec-Websocket-Protocol", "graphql-transport-ws")
@@ -62,6 +66,31 @@ func wshandle(c echo.Context) error {
 		if string(msg) == `{"type":"connection_init","payload":{}}` {
 			if err := ws.WriteMessage(websocket.TextMessage, []byte(`{"type":"connection_ack"}`)); err != nil {
 				c.Logger().Error(err)
+			}
+		} else {
+			gqschema :=  graphql.MustParseSchema(schema, &Resolver{})
+			type SubscribeMessage struct {
+				Id string `json:"id"`
+				Payload struct {
+					Query string `json:"query"`
+				} `json:"payload"`
+			}
+			var message SubscribeMessage
+			if err := json.Unmarshal(msg, &message); err != nil {
+				c.Logger().Error(err)
+			}
+			ch, err := gqschema.Subscribe(context.Background(), message.Payload.Query, "", nil)
+			if err != nil {
+				c.Logger().Error(err)
+			}
+			for {
+				a, _ := <-ch
+				b, _ := json.Marshal(a)
+				fmt.Println(string(b))
+				res := fmt.Sprintf(`{"payload":%s,"id":"%s","type":"next"}`, b, message.Id)
+				if err := ws.WriteMessage(websocket.TextMessage, []byte(res)); err != nil {
+					c.Logger().Error(err)
+				}
 			}
 		}
 	}
