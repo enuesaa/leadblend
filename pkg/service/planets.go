b76e64eaaa938b3f1eef0ff3663c291e1c3aaebf
nua <enuesaa@gmail.com>
2024-04-29 15:11:17 +0900

    feat(usecase): create planet service and very simply usecase

---
+++ pkg/service/planets.go
@@ -0,0 +1,19 @@
+package service
+
+import "github.com/enuesaa/leadblend/pkg/repository"
+
+type Planet struct {}
+
+func NewPlanetService(repos repository.Repos) PlanetService {
+	return PlanetService{
+		repos: repos,
+	}
+}
+
+type PlanetService struct {
+	repos repository.Repos
+}
+
+func (srv *PlanetService) List() ([]Planet, error) {
+	return make([]Planet, 0), nil
+}
