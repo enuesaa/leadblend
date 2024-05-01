73a856e441aa37852952678e04b556b13742277b
nua <enuesaa@gmail.com>
2024-05-01 19:51:29 +0900

    feat(graphql): create record

--- pkg/graph/resolver_create_planet.go
+++ pkg/graph/resolver_create_planet.go
@@ -1,9 +1,32 @@
 package graph
 
+import (
+	"context"
+
+	"github.com/enuesaa/leadblend/pkg/repository/dbq"
+)
+
 type CreatePlanetArgs struct {
 	Name string
 }
 func (r *Resolver) CreatePlanet(args CreatePlanetArgs) (*string, error) {
 	id := "a"
+
+	if err := r.repos.DB.Open(r.db); err != nil {
+		return &id, err
+	}
+
+	query, err := r.repos.DB.Query()
+	if err != nil {
+		return &id, err
+	}
+
+	record := dbq.CreatePlanetParams{
+		Name: args.Name,
+	}
+	if _, err := query.CreatePlanet(context.Background(), record); err != nil {
+		return &id, nil
+	}
+	
 	return &id, nil
 }
