1a0997def40e737ceda9c69aea87c2a145753b1f
nua <enuesaa@gmail.com>
2024-05-02 03:39:37 +0900

    feat(ui): use tailwind css

--- ui/src/routes/+layout.svelte
+++ ui/src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { Client, setContextClient, cacheExchange, fetchExchange } from '@urql/svelte'
+	import './app.css'
 
 	const client = new Client({
 		url: 'http://localhost:3000/graphql',
