c174ae0a57012abceb52c817df7ed852a47a90b8
nua <enuesaa@gmail.com>
2024-05-03 16:40:01 +0900

    feat(ui): create planet, island pages.

---
+++ ui/routes/planets/[planet]/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import { page } from '$app/stores'
+	import { get } from 'svelte/store'
+
+	const planetName = get(page).params.planet
+</script>
---
+++ ui/routes/planets/[planet]/islands/[island]/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { page } from '$app/stores'
+	import { get } from 'svelte/store'
+
+	const params = get(page).params
+	const planetName = params.planet
+	const islandId = params.island
+</script>
+
+planet: {planetName} <br />
+island: {islandId}
