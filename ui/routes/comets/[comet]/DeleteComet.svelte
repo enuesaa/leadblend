faa1543c4510b42971b56e13bcdf9276e111ae8c
nua <enuesaa@gmail.com>
2024-05-06 03:26:11 +0900

    feat(ui): list islands on comet update page

--- ui/routes/comets/[comet]/DeleteComet.svelte
+++ ui/routes/comets/[comet]/DeleteComet.svelte
@@ -12,4 +12,8 @@
 	}
 </script>
 
-<Button {handleClick} label="Delete" />
+<section class="my-6 p-4 bg-grayblack w-fit rounded-lg">
+	<h6 class="text-lg font-bold mb-2">Danger Zone</h6>
+	<span class="mr-3">Are you sure to delete this comet?</span>
+	<Button {handleClick} label="Delete" />
+</section>
--- ui/routes/comets/[comet]/LinkComet.svelte
+++ ui/routes/comets/[comet]/LinkComet.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
 	import Button from '$lib/components/Button.svelte'
-	import TextInput from '$lib/components/TextInput.svelte'
 	import { useLinkComet } from '$lib/graphql/comet'
 	import { goto } from '$app/navigation'
+	import { listPlanets } from '$lib/graphql/planet'
+	import { listIslands } from '$lib/graphql/island'
 	export let cometId: string
 
 	const linkComet = useLinkComet()
 
+	let planetId: string = ''
+	const planets = listPlanets()
+
 	let islandId: string = ''
+	let islands = listIslands(planetId)
+	$: islands = listIslands(planetId)
 
 	async function handleClick() {
 		await $linkComet.mutateAsync({ id: cometId, islandId })
@@ -15,5 +21,35 @@
 	}
 </script>
 
-<TextInput bind:value={islandId} label="Island Id" />
-<Button {handleClick} label="Link" />
+<section class="my-6 p-4 bg-grayblack w-fit rounded-lg">
+	<h6 class="text-lg font-bold mb-2">Link Island</h6>
+	<label>
+		<span>Planet</span>
+		<select bind:value={planetId}>
+			{#each $planets.data ?? [] as planet}
+				<option value={planet.id}>{planet.name}</option>
+			{/each}
+		</select>
+	</label>
+	<label>
+		<span>Island</span>
+		<select bind:value={islandId}>
+			{#each $islands.data ?? [] as island}
+				<option value={island.id}>{island.title}</option>
+			{/each}
+		</select>
+	</label>
+	<Button {handleClick} label="OK" />	
+</section>
+
+<style lang="postcss">
+	label {
+		@apply inline-block mx-1 py-1;
+	}
+	label span {
+		@apply ml-2;
+	}
+	select {
+		@apply w-28 bg-graywhite block m-2 p-2 font-normal rounded text-black outline-none;
+	}
+</style>
