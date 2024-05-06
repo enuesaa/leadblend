c673d90045b9ee64ef2162877a5cff84df469848
nua <enuesaa@gmail.com>
2024-05-06 23:15:55 +0900

    feat(comet): 親子構造が分からなくなるのでネストされた時は親のキーも表示する

--- ui/routes/comets/new/FieldEditor.svelte
+++ ui/routes/comets/new/FieldEditor.svelte
@@ -8,10 +8,20 @@
 
 {#each data.values as item}
 	{#if item.type === 'object'}
-		<svelte:self bind:data={item} />
+		<div>
+			{item.key}
+			<svelte:self bind:data={item} />
+		</div>
 	{:else if item.type === 'array'}
 		<FieldEditorArray bind:data={item} />
 	{:else}
 		<FieldEditorValue bind:data={item} />
 	{/if}
 {/each}
+
+<style lang="postcss">
+	div {
+		border: solid 1px;
+		@apply border-blackgray block w-3/4 p-3 rounded-lg ml-2 my-1;
+	}
+</style>
--- ui/routes/comets/new/FieldEditorArray.svelte
+++ ui/routes/comets/new/FieldEditorArray.svelte
@@ -6,12 +6,22 @@
 	export let data: CometArray
 </script>
 
-{#each data.values as item}
-	{#if item.type === 'object'}
-		<FieldEditor bind:data={item} />
-	{:else if item.type === 'array'}
-		<svelte:self bind:data={item} />
-	{:else}
-		<FieldEditorValue bind:data={item} />
-	{/if}
-{/each}
+<div>
+	{data.key}
+	{#each data.values as item}
+		{#if item.type === 'object'}
+			<FieldEditor bind:data={item} />
+		{:else if item.type === 'array'}
+			<svelte:self bind:data={item} />
+		{:else}
+			<FieldEditorValue bind:data={item} />
+		{/if}
+	{/each}
+</div>
+
+<style lang="postcss">
+	div {
+		border: solid 1px;
+		@apply border-blackgray block w-3/4 p-3 rounded-lg ml-2 my-1;
+	}
+</style>
