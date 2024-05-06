1243c849163f08d7b754d417c8b1a82bdb91697e
nua <enuesaa@gmail.com>
2024-05-06 20:38:55 +0900

    feat(comet): enable to convert comet data to json string even if they are nested or in array

--- ui/lib/comet/tojson.ts
+++ ui/lib/comet/tojson.ts
@@ -1,17 +1,36 @@
-import { type CometObject } from './types'
+import { type CometArray, type CometObject, type CometValue } from './types'
 
 export const convertCometDataToJson = (cometdata: CometObject): string => {
-  const jsondata: any = {}
-
-  for (const child of cometdata.values) {
-    if (child.type === 'array') {
-      continue;
-    }
-    if (child.type === 'object') {
-      continue;
-    }
-    jsondata[child.key] = child.value
-  }
-
+  const jsondata = convertObject(cometdata)
   return JSON.stringify(jsondata)
+}
+
+const convertArray = (data: CometArray): any[] => {
+  const ret: any[] = []
+
+  for (const value of data.values) {
+    if (value.type === 'array') {
+      ret.push(convertArray(value))
+    } else if (value.type === 'object') {
+      ret.push(convertObject(value))
+    } else {
+      ret.push(value.value)
+    }
+  }
+  return ret
+}
+
+const convertObject = (data: CometObject): any => {
+  const ret: any = {}
+
+  for (const value of data.values) {
+    if (value.type === 'array') {
+      ret[value.key] = convertArray(value)
+    } else if (value.type === 'object') {
+      ret[value.key] = convertObject(value)
+    } else {
+      ret[value.key] = value.value
+    }
+  }
+  return ret
 }
