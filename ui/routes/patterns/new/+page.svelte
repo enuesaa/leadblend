7a9230002d2b111a0e65ae21c70784aaea0e1363
nua <enuesaa@gmail.com>
2024-05-09 02:41:34 +0900

    feat(ui): create new pattern

--- ui/routes/patterns/new/+page.svelte
+++ ui/routes/patterns/new/+page.svelte
@@ -5,17 +5,23 @@
 	import PageTitle from '$lib/components/PageTitle.svelte'
 	import { useCreatePattern } from '$lib/graphql/pattern'
 	import type { TraitInput } from '$lib/graphql/types'
+	import TraitForm from './TraitForm.svelte'
 
 	const createPattern = useCreatePattern()
 
 	let title: string = ''
+	let traits: TraitInput[] = []
+
+	function handleAddTrait() {
+		traits = [...traits, {
+			defaultValue: '',
+			path: '$',
+			required: true,
+			type: 'string',
+		}]
+	}
 
 	async function handleClick() {
-		const traits = [
-			{ path: '$.a', type: 'string', defaultValue: 'a', required: true },
-			{ path: '$.b', type: 'string', defaultValue: 'b', required: true },
-			{ path: '$.c', type: 'string', defaultValue: 'c', required: true },
-		] satisfies TraitInput[]
 		await $createPattern.mutateAsync({ title, traits, color: '#1a1a1a' })
 		goto('/')
 	}
@@ -23,4 +29,12 @@
 
 <PageTitle title="New Pattern" />
 <TextInput bind:value={title} label="title" />
+
+<div class="m-3">
+	{#each traits.flat() as trait, i}
+		<TraitForm bind:trait={trait} remove={() => traits = traits.splice(i, 1)} />
+	{/each}
+	<Button handleClick={handleAddTrait} label='add trait' />
+</div>
+
 <Button {handleClick} label="Create" />
---
+++ ui/routes/patterns/new/TraitForm.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import type { TraitInput } from '$lib/graphql/types'
+	import TextInput from '$lib/components/TextInput.svelte'
+	import Button from '$lib/components/Button.svelte'
+
+	export let trait: TraitInput
+	export let remove: () => void
+</script>
+
+<section>
+	<TextInput bind:value={trait.path} label='path' />
+	<label>
+		type
+		<select bind:value={trait.type}>
+			<option value="string">string</option>
+			<option value="boolean">boolean</option>
+			<option value="number">number</option>
+			<option value="array">array</option>
+			<option value="object">object</option>
+			<option value="null">null</option>
+		</select>
+	</label>
+	<TextInput bind:value={trait.defaultValue} label='defaultValue' />
+
+	<label>
+		required
+		<input type="checkbox" bind:value={trait.required}/>
+	</label>
+
+	<div>
+		<Button handleClick={remove} label='remove' />
+	</div>
+</section>
+
+<style lang="postcss">
+	section {
+		border: solid 1px;
+		@apply border-gray px-5 py-2;
+	}
+	select {
+		@apply w-28 block m-2 p-2 font-normal rounded text-black outline-none;
+	}
+</style>
